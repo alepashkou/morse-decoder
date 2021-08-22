c41e3bda966d6125b20f29600a7f029773fcafbb
Alexander Pashkou <ale.pashkou@gmail.com>
2021-08-23 00:52:25 +0300

    init: Start task

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
+
+function decode(expr) {
+
+let result = [ ];
+
+    for(i = 0; i < expr.length; i = i + 10){
+
+        let part = expr.slice(i, i + 10)
+
+            if (part === '**********'){
+               
+                result.push(' ')
+
+            } else {
+
+                let binaryToMorse = part.replace(/00/g,'').replace(/10/g,'.').replace(/11/g,'-')
+                
+                result.push(MORSE_TABLE[binaryToMorse])
+
+            }
+    }
+
+    return result.join('');
+}
+
+module.exports = {
+    decode
+}
